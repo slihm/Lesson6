4db201127276c62432e541fbfaf2f008f9233b03
stev6540 <stev6540@ISS147D8-01.scdsb.pri>
2014-10-02 08:42:20 -0400

    Initial commit

---
+++ src/MakeEmployees.java
@@ -0,0 +1,12 @@
+import TerminalIO.*;
+
+public class MakeEmployees {
+
+    
+    public static void main(String[] args) {
+        KeyboardReader kr = new KeyboardReader();
+        String name = kr.readLine("Enter name ->");
+        System.out.println("The name is: "+ name);
+    }
+    
+}
